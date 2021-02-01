adda23bccbd390a942ee2f2cd345bc8cda304cf1
weldsh <weldsh2535@gmail.com>
2021-02-01 04:38:22 -0800

    update line item
    Signed-off-by: weldsh <weldsh2535@gmail.com>

--- MTK-Inv/src/app/line-items/line-items.page.ts
+++ MTK-Inv/src/app/line-items/line-items.page.ts
@@ -67,7 +67,6 @@ export class LineItemsPage implements OnInit {
       this.dialogRef.close();
     }
   }
-
   validateForm(formData: LineItem) {
     this.isValid = true;
     if (formData.ItemID == 0)
