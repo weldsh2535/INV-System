501d4d4bc276abbf5e06e0a82e165a7be80bb00e
weldsh <weldsh2535@gmail.com>
2021-02-04 23:28:08 -0800

    update
    Signed-off-by: weldsh <weldsh2535@gmail.com>

--- MTK-Inv/src/app/app.component.ts
+++ MTK-Inv/src/app/app.component.ts
@@ -90,7 +90,7 @@ export class AppComponent implements OnInit {
   public reportPage = [
     {
       tittle: 'Report ',
-      url: '',
+      url: '/',
       icon: 'add-circle',
       subPages: [{ title: 'Product Price list', url: '' },
       { title: 'Product Sales', url: '' },
