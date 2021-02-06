b60cef948eb7736d78063a6f681f886f3d21b6b3
weldsh <weldsh2535@gmail.com>
2021-02-06 01:15:33 -0800

    update
    Signed-off-by: weldsh <weldsh2535@gmail.com>

--- MTK-Inv/src/app/report/Sale/product-price-list/product-price-list.page.ts
+++ MTK-Inv/src/app/report/Sale/product-price-list/product-price-list.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
+import { ItemsService } from 'src/app/Service/items.service';
 
 @Component({
   selector: 'app-product-price-list',
@@ -6,10 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-price-list.page.scss'],
 })
 export class ProductPriceListPage implements OnInit {
-
-  constructor() { }
+  itemsOfList: any[];
+  a: number=0;
+  private loading;
+  generateList:boolean=true;
+  constructor(private itemsService:ItemsService,
+    private loadingController:LoadingController) { }
 
   ngOnInit() {
   }
-
+  getItemList(){
+    this.itemsService.getAllItem().subscribe(itemList=>{
+      if(this.a==0){
+        this.itemsOfList=null
+      }
+      this.itemsOfList = itemList;
+      this.generateList=false;
+    })
+  }
+  generate(){
+  this.presentLoading();
+  }
+  async presentLoading() {
+    const loading = await this.loadingController.create({
+      message: 'Please wait...'
+    }).then((overlay)=>{
+      this.loading =overlay
+      this.loading.present();
+    });
+  setTimeout(()=>{
+  this.loading.dismiss();
+  this.a=1
+  this.getItemList();
+  },4000);
+  }
 }
