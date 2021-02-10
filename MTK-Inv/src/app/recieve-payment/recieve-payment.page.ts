5e7c3a3592625c60bc2a8db46c80a398b0b70fc1
weldsh <weldsh2535@gmail.com>
2021-02-10 03:41:07 -0800

    update
    Signed-off-by: weldsh <weldsh2535@gmail.com>

--- MTK-Inv/src/app/recieve-payment/recieve-payment.page.ts
+++ MTK-Inv/src/app/recieve-payment/recieve-payment.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder,FormControl, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { AlertController } from '@ionic/angular';
 import { balance, Customer, Vocher } from 'src/Tabels/tabels-list';
 import { CustomerService } from '../Service/customer.service';
@@ -27,16 +27,16 @@ export class RecievePaymentPage implements OnInit {
   customerId: string;
   totalBalance: number;
   balance: number;
-  payment:number=0;checkedItems = [];
+  payment: number = 0; checkedItems = [];
   constructor(private fb: FormBuilder,
-              private customerService:CustomerService,
-              private alertController:AlertController,
-              private voucherService:VocherService) { }
+    private customerService: CustomerService,
+    private alertController: AlertController,
+    private voucherService: VocherService) { }
   ngOnInit() {
-    this.regform=this.fb.group({
-      customer:["" ,Validators.required],
-      payment:["", Validators.required],
-      updateBalance:0
+    this.regform = this.fb.group({
+      customer: ["", Validators.required],
+      payment: ["", Validators.required],
+      updateBalance: 0
     })
     this.getAllCustomerList();
     //this.getVoucherById();
@@ -44,107 +44,112 @@ export class RecievePaymentPage implements OnInit {
   public fields: Object = { text: "phonenumber", value: "fullname" };
   public watermark2: string = "Select Customer";
   public height: string = "250px";
-  onChange(item , index){
-    if(this.checkedItems.includes(item)) {
-      this.checkedItems = this.checkedItems.filter((value)=>value!=item);
-     } else {
-      this.checkedItems.push(item)
-      this.payment =this.payment+item.subTotal;
-      console.log(index);
+  onChange(item, index) {
+    if (this.checkedItems.includes(item)) {
+      this.checkedItems = this.checkedItems.filter((value) => value != item);
     }
+    else {
+      this.checkedItems.push(item)
+      this.checkedItems.forEach(list => {
+        this.payment = this.payment + list.subTotal;
+      })
+      // console.log(this.checkedItems);
+      // this.payment =this.payment+item.subTotal;
+    }
+    this.checkedItems.forEach(list => {
+      this.payment = this.payment + list.subTotal;
+    });
   }
-  
+
   getDisplayExpr(item) {
     if (!item) {
       return "";
     }
     return item.fullname + " " + item.phonenumber;
   }
-  getAllCustomerList(){
-    this.customerService.getAllCustomer().subscribe(res=>{
-      this.listOfCustomer=res;
-     this.defaultSelectedCurrency = this.listOfCustomer[0].id;
-     this.customerId = this.defaultSelectedCurrency;
-     if(this.customerId!==null){
-      this.voucherService.getVocherByCustomerID(this.customerId).subscribe(res=>{
-        this.listOfVoucher = res;
-      } )
-    }
-   })
+  getAllCustomerList() {
+    this.customerService.getAllCustomer().subscribe(res => {
+      this.listOfCustomer = res;
+      this.defaultSelectedCurrency = this.listOfCustomer[0].id;
+      this.customerId = this.defaultSelectedCurrency;
+      if (this.customerId !== null) {
+        this.voucherService.getVocherByCustomerID(this.customerId).subscribe(res => {
+          this.listOfVoucher = res;
+        })
+      }
+    })
   }
-  getVoucherById(customerId:string){
-    this.totalBalance=0;this.balance=0;
-    if(this.customerId!==null){
-      this.voucherService.getVocherByCustomerID(customerId).subscribe(res=>{
+  getVoucherById(customerId: string) {
+    this.totalBalance = 0; this.balance = 0;
+    if (this.customerId !== null) {
+      this.voucherService.getVocherByCustomerID(customerId).subscribe(res => {
         this.listOfVoucher = res;
-        if(res.length==0){
+        if (res.length == 0) {
           this.balance = this.updateBalance;
         }
-        else{
+        else {
           this.listOfVoucher.forEach(element => {
             this.totalBalance = +this.totalBalance + element.subTotal
-            this.balance = this.totalBalance+this.updateBalance;
-           });
+            this.balance = this.totalBalance + this.updateBalance;
+          });
         }
         //console.log(res)
-      } )
+      })
     }
-   
+
   }
-  onKey(){
-    this.Balance= this.updateBalance + this.totalBalance - this.regform.get("payment").value;
-     }
-  SelectedValue($event){
+  onKey() {
+    this.Balance = this.updateBalance + this.totalBalance - this.regform.get("payment").value;
+  }
+  SelectedValue($event) {
     const newValue = $event.value;
     const previousValue = $event.previousValue;
     this.id = newValue || previousValue;
-    this.filterCustomer = this.listOfCustomer.filter(c=>c.id == this.id);
-      this.selectedCustomerBalance = this.filterCustomer[0].balance;
-      this.updateBalance = this.selectedCustomerBalance;
-      this.customerId = this.filterCustomer[0].id;
-      this.getVoucherById(this.customerId);
+    this.filterCustomer = this.listOfCustomer.filter(c => c.id == this.id);
+    this.selectedCustomerBalance = this.filterCustomer[0].balance;
+    this.updateBalance = this.selectedCustomerBalance;
+    this.customerId = this.filterCustomer[0].id;
+    this.getVoucherById(this.customerId);
   }
-  
-  reCalculateBalance(){
+
+  reCalculateBalance() {
     {
-      if(this.regform.valid)
-      {
-        if(!this.recieveBalanceId)
-        { 
-          let updateCustomerData ={
-            fullname:this.filterCustomer[0].fullname,
-            phonenumber:this.filterCustomer[0].phonenumber,
-            location:this.filterCustomer[0].location,
-            balance:this.balance-this.regform.get("payment").value,
-            address:this.filterCustomer[0].address
-           }
-           this.customerService.updateCustomer(updateCustomerData,this.defaultSelectedCurrency).then(
+      if (this.regform.valid) {
+        if (!this.recieveBalanceId) {
+          let updateCustomerData = {
+            fullname: this.filterCustomer[0].fullname,
+            phonenumber: this.filterCustomer[0].phonenumber,
+            location: this.filterCustomer[0].location,
+            balance: this.balance - this.regform.get("payment").value,
+            address: this.filterCustomer[0].address
+          }
+          this.customerService.updateCustomer(updateCustomerData, this.defaultSelectedCurrency).then(
             () =>
-            (error: AppError) => {
-              if (error instanceof BadInput) {
-              this.regform.setErrors(error.originalError);
+              (error: AppError) => {
+                if (error instanceof BadInput) {
+                  this.regform.setErrors(error.originalError);
+                }
+                else throw error;
               }
-              else throw error;
-            }
           );
         }
         this.regform.reset();
-        this.recieveBalanceId="";
+        this.recieveBalanceId = "";
         this.presentAlert(" Sucess");
       }
-      else{
-       this.presentAlert("Please enter all fields");
+      else {
+        this.presentAlert("Please enter all fields");
       }
     }
   }
-    async presentAlert(message) {
-     const alert = await this.alertController.create({
-       cssClass: 'my-custom-class',
-       header: 'Recive payment',
-       // subHeader: 'Subtitle',
-       message: message,
-       buttons: ['OK']
-     });
-     await alert.present();
-   }
+  async presentAlert(message) {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Recive payment',
+      // subHeader: 'Subtitle',
+      message: message,
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
 }
